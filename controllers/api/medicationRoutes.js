dc4f49f68e72e23d16698ac6d537e161ae8b16ed
Amelia Hooper <ajhooper97@gmail.com>
2021-11-29 18:45:00 +1100

    Created medication route

---
+++ controllers/api/medicationRoutes.js
@@ -0,0 +1,38 @@
+const router = require('express').Router();
+const { User, Medication } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.post('/', withAuth, async (req, res) => {
+    try {
+      const newMedication = await Medication.create({
+        ...req.body,
+        user_id: req.session.user_id,
+      });
+  
+      res.status(200).json(newMedication);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+
+  router.delete('/:id', withAuth, async (req, res) => {
+    try {
+      const medicationData = await Medication.destroy({
+        where: {
+          id: req.params.id,
+          user_id: req.session.user_id,
+        },
+      });
+  
+      if (!medicationData) {
+        res.status(404).json({ message: 'No medication found with this id!' });
+        return;
+      }
+  
+      res.status(200).json(projectData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  module.exports = router;
