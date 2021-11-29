1154a88e476315f2602fc2ea727621ff29c604c8
Anna <an9062@gmail.com>
2021-11-29 21:32:44 +1100

    linked handlebars to index routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,20 @@
-const router = require('express').Router();
-const apiRoutes = require('./api');
+const router = require("express").Router();
+const apiRoutes = require("./api");
 // const homeRoutes = require('./homeRoutes');
 
 // router.use('/', homeRoutes);
-router.get('/', (req, res) => {
-    res.render('homepage');
+router.get("/", (req, res) => {
+  res.render("login");
 });
-router.use('/api', apiRoutes);
+
+router.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
+router.use("/api", apiRoutes);
 
 module.exports = router;
