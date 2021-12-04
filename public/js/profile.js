b45c89c3288e2e53f1bab2e8a878012f28486a69
Anna <an9062@gmail.com>
2021-12-04 22:28:31 +1100

    added delete function

--- public/js/profile.js
+++ public/js/profile.js
@@ -4,11 +4,6 @@ function noBackground() {
   body.style.background = "none";
 }
 
-// const getMedSchedule = async () => {
-//   const response = await fetch("/api/profile/");
-//   console.log(response);
-// };
-
 const newMedSchedule = async (event) => {
   event.preventDefault();
 
@@ -41,8 +36,28 @@ const newMedSchedule = async (event) => {
   }
 };
 
+const delButtonHandler = async (event) => {
+  if (event.target.hasAttribute("data-id")) {
+    const id = event.target.getAttribute("data-id");
+
+    const response = await fetch(`/api/medication/${id}`, {
+      method: "DELETE",
+    });
+
+    if (response.ok) {
+      document.location.replace("/profile");
+    } else {
+      alert("Failed to delete medication");
+    }
+  }
+};
+
 document
   .querySelector(".new-schedule")
   .addEventListener("submit", newMedSchedule);
+
+document
+  .querySelector(".medication-list")
+  .addEventListener("click", delButtonHandler);
 
 noBackground();
