5b4d5b427368ae647c40b64da2d85309dacec016
Amelia Hooper <ajhooper97@gmail.com>
2021-12-06 18:11:12 +1100

    Heroku port

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
-const PORT = (process.env.PORT || 3001, function () {
+const PORT = (process.env.PORT || 8080, function () {
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
